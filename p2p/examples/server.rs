61f85f77dc11598fce5f6732395e4f227eb15e5e
Song Xuyang <xuyangsong@hotmail.com>
2021-09-09 11:48:47 +0800

    fix test error

--- p2p/examples/server.rs
+++ p2p/examples/server.rs
@@ -1,5 +1,5 @@
 use p2p::Message;
-use p2p::{MsgProcess, Node, ProcessMessage};
+use p2p::{Info, MsgProcess, Node, ProcessMessage};
 use tokio::task;
 struct TestReceive {}
 impl MsgProcess<Message> for TestReceive {
@@ -13,13 +13,13 @@ fn main() {
     let mut rt = tokio::runtime::Runtime::new().expect("Should be able to init tokio::Runtime.");
     let local = task::LocalSet::new();
     local.block_on(&mut rt, async move {
-        let index: usize = 0;
-        let ip = "127.0.0.1".parse().unwrap();
-        let port = 64000;
+        let my_info = Info {
+            index: 0,
+            address: "127.0.0.1:64000".to_string(),
+        };
 
         //Init a node as server
-        let (mut node_handle, notifications_channel) =
-            Node::<Message>::node_init(index, ip, port).await;
+        let (mut node_handle, notifications_channel) = Node::<Message>::node_init(&my_info).await;
 
         let mut message_process = TestReceive {};
         //Receive messages
